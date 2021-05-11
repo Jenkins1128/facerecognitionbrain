759b4b4b50a5036e782ee5afd03c6e9e2b26233c
Isaiah Jenkins <icjenkins@dons.usfca.edu>
2021-05-11 11:24:10 -0700

    deploy

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -17,7 +17,7 @@ class Signin extends React.Component {
 	};
 
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3000/signin', {
+		fetch('https://radiant-castle-26036.herokuapp.com/signin', {
 			method: 'post',
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify({
